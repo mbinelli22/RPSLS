d1f926a5b5a0d930580385a29b26558a079b659e
mbinelli22 <mbinelli1996@gmail.com>
2017-10-06 15:24:39 -0500

    playgame layout

--- RSPLS/RPSLS.cs
+++ RSPLS/RPSLS.cs
@@ -8,6 +8,60 @@ namespace RSPLS
 {
     class RPSLS
     {
+        //- Create a console application for Rock, Paper, Scissors, Lizard, Spock
+        //- Your game should have the option of Single player(vs AI) or 2 player(Human vs Human)
+        //- Your game must be a minimum of best of three to decide a winner
+        //Rules for Rock, Paper, Scissors, Lizard, Spock:
+        //Scissors cuts Paper
+        //Paper covers Rock
+        //Rock crushes Lizard
+        //Lizard poisons Spock
+        //Spock smashes Scissors
+        //Scissors decapitates Lizard
+        //Lizard eats Paper
+        //Paper disproves Spock
+        //Spock vaporizes Rock
+        //(and as it always has) Rock crushes Scissors
+        
+        //memeber variables
+
+        //constructor
+        public void playGame()
+        {
+            Console.WriteLine("");
+            string shoot = Console.ReadLine();
+            switch (shoot)
+            {
+                case "rock":
+
+                    break;
+                case "paper":
+
+                    break;
+                case "scissors":
+
+                    break;
+                case "lizzard":
+
+                    break;
+                case "spock":
+
+                    break;
+
+            }
+
+        }
+
+        //member methods
+        public void playerOne()
+        {
+
+        }
+    
+        public void playerTwo()
+        {
+
+        }
 
     }
 }
