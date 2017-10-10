032eb20e08c3dd9a62112d2cd381ad4e943e8cb0
mbinelli22 <mbinelli1996@gmail.com>
2017-10-10 13:00:30 -0500

    added getRandomThrow function along with multiplayer and single player classes

--- RSPLS/Program.cs
+++ RSPLS/Program.cs
@@ -10,6 +10,10 @@ namespace RSPLS
     {
         static void Main(string[] args)
         {
+            RPSLS RSPLS= new RPSLS();
+            RSPLS.singlePlayerOrMultiPlayer();
+            RSPLS.playGame();
+                        
         }
     }
 }
--- RSPLS/RPSLS.cs
+++ RSPLS/RPSLS.cs
@@ -8,8 +8,84 @@ namespace RSPLS
 {
     class RPSLS
     {
+        //member variables
+        public string Rock;
+        public string Paper;
+        public string Scissors;
+        public string Spock;
+        public string Lizard;
+        int randomThrow;
+        int rock = 0;
+        int paper = 1;
+        int scissors = 2;
+        int spock = 3;
+        int lizard = 4;
+
+        //constructor
+        public RPSLS(string rock, string paper, string scissors, string spock, string lizard)
+        {
+
+        }
+
+        //memeber methods
+
+
+
+
         //- Create a console application for Rock, Paper, Scissors, Lizard, Spock
         //- Your game should have the option of Single player(vs AI) or 2 player(Human vs Human)
+        public void singlePlayerOrMultiPlayer()
+        {
+            Console.WriteLine("To play agianst the computer type 'ai'. To play human vs human type 'human'.");
+            string vsDecision = Console.ReadLine();
+            switch (vsDecision)
+            {
+                case "ai":
+                    base(singlePlayer);
+                    break;
+                case "human":
+                    base(multiPlayer);
+                    break;
+            }
+        }
+
+        List<string> throws = new List<string>() {"Rock", "Paper", "Scissors", "Spock", "Lizard"};
+
+
+
+        private void getRandomThrow()
+        {
+            int randomThrow;
+            Random rnd = new Random();
+            randomThrow = rnd.Next(0, 5);
+            switch (randomThrow)
+            {
+                case 0:
+                    Console.WriteLine("Computer threw Rock.");
+                    break;
+                case 1:
+                    Console.WriteLine("Computer threw Paper.");
+                    break;
+                case 2:
+                    Console.WriteLine("Computer threw Scissors.");
+                    break;
+                case 3:
+                    Console.WriteLine("Computer threw Spock.");
+                    break;
+                case 4:
+                    Console.WriteLine("Computer threw Lizard.");
+                    break;
+            }
+          }
+
+
+        
+        // d = (5 + a - b) % 5
+        // d = 1 or d = 3 => a wins
+        // d = 2 or d = 4 => b wins
+        // d = 0 => tie
+
+
         //- Your game must be a minimum of best of three to decide a winner
         //Rules for Rock, Paper, Scissors, Lizard, Spock:
         //Scissors cuts Paper
@@ -22,13 +98,10 @@ namespace RSPLS
         //Paper disproves Spock
         //Spock vaporizes Rock
         //(and as it always has) Rock crushes Scissors
-        
-        //memeber variables
 
-        //constructor
         public void playGame()
         {
-            Console.WriteLine("");
+            Console.WriteLine("Type in what you want to play. \n Rock, Paper, Scissors, Lizard, Spock, SHOOT!");
             string shoot = Console.ReadLine();
             switch (shoot)
             {
@@ -47,21 +120,9 @@ namespace RSPLS
                 case "spock":
 
                     break;
-
-            }
-
+             }
         }
 
-        //member methods
-        public void playerOne()
-        {
-
-        }
-    
-        public void playerTwo()
-        {
-
-        }
 
     }
 }
